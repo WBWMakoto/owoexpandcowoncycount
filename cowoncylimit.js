bbba4ffd1a678bd053f9d914731b89ec35a2680a
Shinji Makoto <57854962+WBWMakoto@users.noreply.github.com>
2023-06-09 15:23:35 +0700

    Create cowoncylimit.js

---
+++ cowoncylimit.js
@@ -0,0 +1,25 @@
+const levels = 75;
+
+async function calculateLimits() {
+  const levelLimits = [];
+
+  for (let level = 1; level <= 75; level++) {
+    const limits = await getUserLimits(level);
+    levelLimits.push({ level, ...limits });
+  }
+
+  console.log(levelLimits);
+}
+
+async function getUserLimits(level) {
+  const lvl = level;
+  const tens = Math.floor(level / 10);
+  const limit = 50000 + lvl * 14000 + tens * 5000000;
+
+  return {
+    send: limit,
+    receive: Math.ceil(limit * (tens / 2 + 1)),
+  };
+}
+
+calculateLimits();
